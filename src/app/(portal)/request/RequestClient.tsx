67269b65ca4249f0a80f3c7124d2746e066ce0d1
fam3boy <fameboyofficial@gmail.com>
2026-04-23 20:43:38 +0900

    Update RequestClient.tsx

--- src/app/(portal)/request/RequestClient.tsx
+++ src/app/(portal)/request/RequestClient.tsx
@@ -205,7 +205,7 @@ export default function RequestClient({ initialTheme, initialBranding }: { initi
   };
 
   return (
-    <div className="bg-white min-h-screen pb-40">
+    <div className="bg-white min-h-[calc(100vh-200px)]">
 
       <div className="portal-container pt-6 md:pt-10">
         {/* Theme Display */}
@@ -449,7 +449,7 @@ export default function RequestClient({ initialTheme, initialBranding }: { initi
                  </div>
               </div>
 
-              <div className="pt-4 md:pt-8 pb-20 md:pb-40 flex flex-col items-center gap-8">
+              <div className="pt-4 md:pt-8 pb-10 flex flex-col items-center gap-8">
                  <div className="p-6 md:p-8 bg-[#f8f8f8] border border-hyundai-gray-100 text-center w-full">
                     <p className="text-[14px] font-medium text-hyundai-gray-500 leading-relaxed">
                        입력하신 정보는 신청곡 관리 목적으로만 사용되며, <br className="hidden md:block" /> 
